8606ad123cbea8e02144a8b5328d11c798b7e729
jadapema <jadapema@gmail.com>
2025-04-26 07:38:13 -0600

    fix: remove comments

--- src/graphql/posts/resolvers/Post.ts
+++ src/graphql/posts/resolvers/Post.ts
@@ -2,11 +2,6 @@ import type { PostResolvers, Post as PostType } from '@/schema/types'
 
 export const Post: PostResolvers = {
     author: (p, _a, { services }) => services.Profile.getProfile(p.author.address),
-
-    // comments:   (p, _a, { services }) => services.Comments.getCommentsByPost(p.id),
-    // likes:      (p, _a, { services }) => services.Social.getPostLikes(p.id),
-    // bookmarks:  (p, _a, { services }) => services.Social.getPostBookmarks(p.id),
-
     replyTo:  (p, _a, { services }) => p.replyTo  ? services.Posts.getPost(p.replyTo)  : null,
     quoteOf:  (p, _a, { services }) => p.quoteOf  ? services.Posts.getPost(p.quoteOf)  : null,
 }
