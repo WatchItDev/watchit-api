73b9fb89eb287bb0bc97aa45734f06d90d9c7f8d
jadapema <jadapema@gmail.com>
2025-04-26 07:45:56 -0600

    fix: remove social null

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,3 +3,6 @@ export * as usersTrigger from './flows/users/triggers';
 
 export * as postsCallable   from './flows/posts/callables';
 export * as postsTrigger    from './flows/posts/triggers';
+
+export * as commentsCallable from './flows/comments/callables';
+export * as commentsTrigger  from './flows/comments/triggers';
