6ec38d581ba1db3a0215481b85291f0596b5e303
Geolffrey Mena <gmjun2000@gmail.com>
2025-05-10 10:36:20 -0600

    changes in server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import 'dotenv/config'
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import { format } from 'url';
 
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4'
+
 import {
     createApollo4QueryValidationPlugin,
     constraintDirectiveTypeDefsGql
@@ -15,31 +22,51 @@ import { DataSources } from './datasources';
 import * as externals from './externals'
 import { GQL } from "@/types";
 
-
-const host = process.env.API_HOST || '0.0.0.0';
-const port = (process.env.API_PORT || 4000) as number
-
-const server = new ApolloServer<GQL.ContextType>({
-    resolvers,
-    typeDefs: [constraintDirectiveTypeDefsGql, typeDefs],
-    plugins: [createApollo4QueryValidationPlugin()],
-})
-
-
 const startServer = async () => {
+
+    const host = process.env.API_HOST || '0.0.0.0';
+    const port = (process.env.API_PORT || 4000) as number
+    const app: express.Express = express();
+    const httpServer: http.Server = http.createServer(app);
+
+    const server = new ApolloServer<GQL.ContextType>({
+        resolvers,
+        typeDefs: [constraintDirectiveTypeDefsGql, typeDefs],
+        plugins: [
+            createApollo4QueryValidationPlugin(),
+            ApolloServerPluginDrainHttpServer({ httpServer: httpServer })
+        ],
+    })
+
+
+    console.log("Starting server..")
+    await server.start();
 
     const fireStore = FireStore();
     const dataSources = DataSources(fireStore);
     const services = Services({ ds: dataSources, ext: externals })
 
-    return await startStandaloneServer(server, {
-        listen: { port, host }, context: ({ req }) => {
-            return { services, dataSources, req }
-        }
-    })
+    app.use(
+        cors(),
+        helmet(),
+        app.disable('x-powered-by'),
+        express.json({ limit: '50mb' }),
+        expressMiddleware(server, {
+            context: (({ req }) => {
+                return { services, dataSources, req }
+            })
+        }),
+    )
+
+    // Wait for server to start listening
+    await new Promise<void>((resolve) => {
+        httpServer.listen({ host, port }, resolve);
+    });
+
+    return format({ protocol: 'http', host, port, pathname: '/', });
 }
 
 // The `listen` method launches a web server
-const { url } = await startServer();
+const url = await startServer();
 console.log(`🚀 Server ready at ${url}`)
 
