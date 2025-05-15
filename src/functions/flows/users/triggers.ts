f065a6cfdb2bd9cc456d9f375746deb1429f1577
jadapema <jadapema@gmail.com>
2025-05-14 20:33:35 -0600

    feat: connect xp dispatch when an account is created

--- src/functions/flows/users/triggers.ts
+++ src/functions/flows/users/triggers.ts
@@ -1,10 +1,27 @@
 import { onDocumentCreated, onDocumentUpdated } from 'firebase-functions/v2/firestore';
+import { enhanceTrigger } from "../../manager";
+import { FirestoreUser } from "../../../externals/firebase/types";
+import { makeXpEntry, XpAction } from "../../../models/xp";
 
 export const logUserCreated = onDocumentCreated(
     'users/{wallet}',
-    (event) => {
-        console.log(`🔥 Trigger fired: user ${event.params.wallet} created`);
-    }
+    enhanceTrigger(async ({ ds }, event) => {
+        const { wallet } = event.params;
+        // Get the payload of the newly created user
+        const newUser = event.data!.data() as FirestoreUser;
+        const beforeBalance = newUser.xpBalance ?? 0;
+
+        // Create and save the XP entry
+        const entry = makeXpEntry({
+            action:      XpAction.REGISTER_BONUS,
+            description: 'User registration',
+            amount:      50,
+            before:      beforeBalance,
+        });
+        await ds.XP.addEntry(wallet, entry);
+
+        console.log(`✨ 50 XP awarded to ${wallet} for completing user registration (previous balance: ${beforeBalance})`);
+    })
 );
 
 export const logUserUpdated = onDocumentUpdated(
