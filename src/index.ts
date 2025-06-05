8d386333dcd23d5c10030124179c1b74d43abed0
Geolffrey Mena <gmjun2000@gmail.com>
2025-06-05 14:41:45 -0600

    ci: added build process

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,7 @@ const startServer = async (): Promise<{ url: string, server: http.Server }> => {
         plugins: [
             createApollo4QueryValidationPlugin(),
             ApolloServerPluginDrainHttpServer({ httpServer }),
-            SentryPlugin()
+            SentryPlugin(), myPlugin
         ],
     })
 
@@ -82,18 +82,18 @@ const startServer = async (): Promise<{ url: string, server: http.Server }> => {
     //     }
     // }
 
-    const limiter = rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-        standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-        legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    })
+    // const limiter = rateLimit({
+    //     windowMs: 15 * 60 * 1000, // 15 minutes
+    //     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    //     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    //     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // })
 
     app.disable('x-powered-by');
     app.use(cors())
     app.use(helmet());
     // app.use(limiter);
-    app.use(express.json({ limit: '50mb' }))
+    // app.use(express.json({ limit: '50mb' }))
     app.use(expressMiddleware<GQL.ContextType>(server, {
         context: async ({ req }: ExpressContextFunctionArgument): Promise<GQL.ContextType> => {
             return { services, dataSources, req, user: {} as User }
