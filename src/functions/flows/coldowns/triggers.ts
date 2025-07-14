0ad37038ad76f5011685b2bbfbbee24f67c00511
jadapema <jadapema@gmail.com>
2025-07-14 15:43:34 -0600

    feat: added achievements

--- src/functions/flows/coldowns/triggers.ts
+++ src/functions/flows/coldowns/triggers.ts
@@ -1,8 +1,11 @@
-import { onSchedule }   from 'firebase-functions/v2/scheduler';
+import { onSchedule } from 'firebase-functions/v2/scheduler';
 import { enhanceFunction } from '../../manager';
 
 export const resetCooldowns = onSchedule(
-    'every 1 hour',
+    {
+        schedule : '0 3 * * *',
+        timeZone : 'UTC',
+    },
     enhanceFunction(async ({ services }) => {
         const n = await services.Perks.resetCooldowns();
         console.log(`⏰  Cooldowns reset: ${n}`);
